265aa7a89adb03b647930c0e1bc065338ef25855
unknown <varuzhangevorgyan5@gmail.com>
2023-03-18 13:51:51 +0400

    add pagination v1

--- packages/frontend/src/app/pages/traffic/Pagination.js
+++ packages/frontend/src/app/pages/traffic/Pagination.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import DATSET from "../../dataset/routesData"
 import { ROUTES_COUNT_FOR_PAGE } from '../../utils/utils'
+import Arrow from "../../icons/arrow.png"
 
-const Pagination = ({currentPage, changePage}) => {
-  return (
-    <div className='pagination-container'>
-        <div className="pagination-pages">
-            {currentPage+1} - {currentPage * ROUTES_COUNT_FOR_PAGE} of {DATSET.length} items
+const Pagination = ({currentPage, setCurrentPage}) => {
+    console.log(DATSET.length , ROUTES_COUNT_FOR_PAGE)
+    const lastRouteIndex =  (currentPage+1) * ROUTES_COUNT_FOR_PAGE > DATSET.length ? DATSET.length: (currentPage+1) * ROUTES_COUNT_FOR_PAGE
+    return (
+        <div className='pagination-container'>
+            <div className="pagination-pages">
+                {currentPage*ROUTES_COUNT_FOR_PAGE} - {lastRouteIndex } of {DATSET.length} items
+            </div>
+            <div className="pagination-itmems-container">
+                <button>
+                    <img src={Arrow} alt="" />
+                </button>
+                        {Array(Math.ceil(DATSET.length / ROUTES_COUNT_FOR_PAGE)).fill((_,i)=>i).map((_,i)=>
+                            <button onClick={()=>setCurrentPage(i)}>
+                                {i+1}
+                            </button>
+                        )}
+                <button>
+                    <img src={Arrow} alt="" />
+                </button>
+            </div>
         </div>
-        <div className="">
-        
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Pagination
--- packages/frontend/src/app/pages/traffic/Traffic.js
+++ packages/frontend/src/app/pages/traffic/Traffic.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./traffic.css";
 import filterIcon from "../../icons/filter.png"
+import searchIcon from "../../icons/search.png"
 import arrow from "../../icons/arrow.png"
 import Pagination from './Pagination';
-
+import DATASET from "../../dataset/routesData"
+import { getPagesRoutes } from '../../utils/utils';
 
 const Traffic = () => {
   const [currentPage, setCurrentPage] = useState(0);
-
+  const [routes, setRoutes] = useState(getPagesRoutes(currentPage))
+  const search = (e)=>{
+    const {value} = e.target
+    const filteredValues = DATASET.filter((route)=>route.Route_ID.toString().startsWith(value))
+    setRoutes(filteredValues)
+  }
+  console.log(routes)
   return (
     <>
     <div className='traffic-top'>
@@ -21,11 +29,13 @@ const Traffic = () => {
     <div className='traffic-table'>
       <div className='traffic-search'>
         <div className='search'>
-          <img src={search} alt="" />
+          <img src={searchIcon} alt="" />
           <input className='search-input' name='search' type="text" placeholder='Search by route ID' />
         </div>
       </div>
       <table>
+        <thead>
+
         <tr className='first-tr' key="">
           <th></th>
           <th>Route ID</th>
@@ -36,16 +46,23 @@ const Traffic = () => {
           <th>Start date</th>
           <th>End date</th>
         </tr>
-        <tr key="">
-          <td>1</td>
-          <td>8074567876543</td>
-          <td>Ney York City, 5th avenue, sq....</td>
-          <td>This status inform</td>
-          <td>12</td>
-          <td>23</td>
-          <td>13 May 2022 - 11:20</td>
-          <td>13 May 2022 - 11:23</td>
-        </tr>
+        </thead>
+        <tbody>
+        {
+          routes.map((route, i)=>
+          <tr key={route.Usage_index}>
+            <td>{i+1}</td>
+            <td>{route.Route_ID}</td>
+            <td>{route.Start_Address}</td>
+            <td>{route.End_address}</td>
+            <td>{route.Workload_index}</td>
+            <td>{route.Usage_index}</td>
+            <td>{route.Start_date}</td>
+            <td>{route.End_date}</td>
+          </tr>
+          )
+        }
+        </tbody>
       </table>
     </div>
     <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage}/>
