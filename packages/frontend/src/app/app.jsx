cda1bd6648cb71aebe84357e9f6aba9b9a452454
unknown <davitmakaryan0404@gmail.com>
2023-03-18 12:54:28 +0400

    add others

--- packages/frontend/src/app/app.jsx
+++ packages/frontend/src/app/app.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "./davo/Layout/Layout";
-import Traffic from "./davo/pages/traffic/Traffic";
+import Layout from "./Layout/Layout";
+import Traffic from "./pages/traffic/Traffic";
 
 export function App() {
 
