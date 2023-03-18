86f6be27e4271bed39842bc6d4523e6b5a417550
unknown <varuzhangevorgyan5@gmail.com>
2023-03-18 14:15:04 +0400

    add/pagination v2

--- packages/frontend/src/app/pages/traffic/Pagination.js
+++ packages/frontend/src/app/pages/traffic/Pagination.js
@@ -1,22 +1,20 @@
 import React from 'react'
-import DATSET from "../../dataset/routesData"
 import { ROUTES_COUNT_FOR_PAGE } from '../../utils/utils'
 import Arrow from "../../icons/arrow.png"
 
-const Pagination = ({currentPage, setCurrentPage}) => {
-    console.log(DATSET.length , ROUTES_COUNT_FOR_PAGE)
-    const lastRouteIndex =  (currentPage+1) * ROUTES_COUNT_FOR_PAGE > DATSET.length ? DATSET.length: (currentPage+1) * ROUTES_COUNT_FOR_PAGE
+const Pagination = ({currentPage, setCurrentPage,data}) => {
+    const lastRouteIndex =  (currentPage+1) * ROUTES_COUNT_FOR_PAGE > data.length ? data.length: (currentPage+1) * ROUTES_COUNT_FOR_PAGE
     return (
         <div className='pagination-container'>
             <div className="pagination-pages">
-                {currentPage*ROUTES_COUNT_FOR_PAGE} - {lastRouteIndex } of {DATSET.length} items
+                {currentPage*ROUTES_COUNT_FOR_PAGE} - {lastRouteIndex } of {data.length} items
             </div>
             <div className="pagination-itmems-container">
-                <button>
+                <button >
                     <img src={Arrow} alt="" />
                 </button>
-                        {Array(Math.ceil(DATSET.length / ROUTES_COUNT_FOR_PAGE)).fill((_,i)=>i).map((_,i)=>
-                            <button onClick={()=>setCurrentPage(i)}>
+                        {Array(Math.ceil(data.length / ROUTES_COUNT_FOR_PAGE)).fill((_,i)=>i).map((_,i)=>
+                            <button onClick={()=>setCurrentPage(i)} key={i}>
                                 {i+1}
                             </button>
                         )}
--- packages/frontend/src/app/pages/traffic/Traffic.js
+++ packages/frontend/src/app/pages/traffic/Traffic.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import "./traffic.css";
 import filterIcon from "../../icons/filter.png"
 import searchIcon from "../../icons/search.png"
@@ -6,16 +6,32 @@ import arrow from "../../icons/arrow.png"
 import Pagination from './Pagination';
 import DATASET from "../../dataset/routesData"
 import { getPagesRoutes } from '../../utils/utils';
+import useDebounce from '../../hooks/useDebouce';
 
 const Traffic = () => {
   const [currentPage, setCurrentPage] = useState(0);
-  const [routes, setRoutes] = useState(getPagesRoutes(currentPage))
+  const [dataset, setDataset] = useState(DATASET)
+  const [routes, setRoutes] = useState(getPagesRoutes(currentPage,dataset))
+  const [value, setValue] = useState('') 
+  const debouncedValue = useDebounce(value, 1000)
+
   const search = (e)=>{
     const {value} = e.target
-    const filteredValues = DATASET.filter((route)=>route.Route_ID.toString().startsWith(value))
-    setRoutes(filteredValues)
+    setValue(value)
   }
-  console.log(routes)
+  
+  useEffect(() => {
+    const filteredValues = dataset.filter((route)=>route.Route_ID.toString().startsWith(debouncedValue))
+    setCurrentPage(0)
+    setDataset(filteredValues)  
+  }, [debouncedValue])
+console.log(routes);
+  useEffect(() => {
+    setRoutes(getPagesRoutes(currentPage, dataset))
+  }, [dataset, currentPage])
+  
+  
+  
   return (
     <>
     <div className='traffic-top'>
@@ -30,7 +46,7 @@ const Traffic = () => {
       <div className='traffic-search'>
         <div className='search'>
           <img src={searchIcon} alt="" />
-          <input className='search-input' name='search' type="text" placeholder='Search by route ID' />
+          <input className='search-input' name='search' type="text" placeholder='Search by route ID' onChange={search} value={value} />
         </div>
       </div>
       <table>
@@ -50,7 +66,7 @@ const Traffic = () => {
         <tbody>
         {
           routes.map((route, i)=>
-          <tr key={route.Usage_index}>
+          <tr key={route.Route_ID}>
             <td>{i+1}</td>
             <td>{route.Route_ID}</td>
             <td>{route.Start_Address}</td>
@@ -65,7 +81,7 @@ const Traffic = () => {
         </tbody>
       </table>
     </div>
-    <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+    <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} data={dataset}/>
     </>
   )
 }
